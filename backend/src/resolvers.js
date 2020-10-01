d2b6c03873edf5f1820828b9b5f8f75cc653ca50
leoreisdias <leoanrdord99@gmail.com>
2020-10-01 14:53:19 -0300

    first config for GraphQL

---
+++ backend/src/resolvers.js
@@ -0,0 +1,21 @@
+const users = [
+    { id: 1, name: "Leonardo", email: "leonardo@gmail.com" },
+    { id: 2, name: "Vanessa", email: "vanessa@gmail.com" },
+]
+
+module.exports = {
+    Query: {
+        users: () => {
+
+        },
+        user: () => {
+
+        }
+    },
+
+    Mutation: {
+        createUser: () => {
+
+        }
+    }
+}
